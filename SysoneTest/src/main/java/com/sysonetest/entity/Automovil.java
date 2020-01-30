c6063f56193cb26bef22bdf721c3753e0b19e2d6
garofoloh <41156618+garofoloh@users.noreply.github.com>
2020-01-29 21:26:00 -0300

    set and get

--- SysoneTest/src/main/java/com/sysonetest/entity/Automovil.java
+++ SysoneTest/src/main/java/com/sysonetest/entity/Automovil.java
@@ -13,4 +13,73 @@ public class Automovil {
 
     ArrayList comodidades;
 
+    public Automovil() {
+        precioBasico = 0;
+        techo = null;
+        aire =null;
+        frenos = null;
+        airbag = null;
+        llantas =null;
+        comodidades = new ArrayList();
+        
+    }
+
+    public int getPrecioBasico() {
+        return precioBasico;
+    }
+
+    public void setPrecioBasico(int precioBasico) {
+        this.precioBasico = precioBasico;
+    }
+
+    public TechoCorredizo getTecho() {
+        return techo;
+    }
+
+    public void setTecho(TechoCorredizo techo) {
+        this.techo = techo;
+    }
+
+    public AireAcondicionado getAire() {
+        return aire;
+    }
+
+    public void setAire(AireAcondicionado aire) {
+        this.aire = aire;
+    }
+
+    public FrenosAbs getFrenos() {
+        return frenos;
+    }
+
+    public void setFrenos(FrenosAbs frenos) {
+        this.frenos = frenos;
+    }
+
+    public Airbag getAirbag() {
+        return airbag;
+    }
+
+    public void setAirbag(Airbag airbag) {
+        this.airbag = airbag;
+    }
+
+    public LlantasAleacion getLlantas() {
+        return llantas;
+    }
+
+    public void setLlantas(LlantasAleacion llantas) {
+        this.llantas = llantas;
+    }
+
+    public ArrayList getComodidades() {
+        return comodidades;
+    }
+
+    public void setComodidades(ArrayList comodidades) {
+        this.comodidades = comodidades;
+    }
+    
+    
+
 }
