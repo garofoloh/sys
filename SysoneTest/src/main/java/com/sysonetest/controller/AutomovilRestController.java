fa98429b6b0c211332b49a1683828f8a7a3c4d0a
garofoloh <41156618+garofoloh@users.noreply.github.com>
2020-01-30 11:41:32 -0300

    delete

--- SysoneTest/src/main/java/com/sysonetest/controller/AutomovilRestController.java
+++ SysoneTest/src/main/java/com/sysonetest/controller/AutomovilRestController.java
@@ -1,7 +1,9 @@
 package com.sysonetest.controller;
 
 import com.sysonetest.entity.Automovil;
+import com.sysonetest.entity.Comodidad;
 import com.sysonetest.service.AutomovilService;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -38,8 +41,8 @@ public class AutomovilRestController {
     @GetMapping("/listadoAutomovilPorId/{id}")
     public Automovil indexId(@PathVariable Long id) {
         Automovil auto = this.automovilService.findById(id);
- 
-       auto.calcularPrecio(auto, auto.getComodidades());
+
+        auto.calcularPrecio(auto, auto.getComodidades());
 
         return auto;
     }
@@ -65,4 +68,17 @@ public class AutomovilRestController {
         return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
     }
 
+    @PostMapping("/updateAutomivil/{id}")
+    public Automovil updateAutomovil(@RequestBody Automovil automovil, @PathVariable Long id) {
+        Automovil automovilActual = this.automovilService.findById(id);
+        return this.automovilService.updateAutomovil(automovilActual, automovil);
+    }
+    @PostMapping("/deleteAutomivil/{id}")
+    public void deleteAutomovil(@PathVariable Long id) {
+        Automovil automovilActual = this.automovilService.findById(id);
+        List<Comodidad> comodidades = new ArrayList<Comodidad>();
+        automovilActual.setComodidades(comodidades);
+         this.automovilService.deleteAutomovil(automovilActual);
+
+}
 }
--- SysoneTest/src/main/java/com/sysonetest/service/AutomovilService.java
+++ SysoneTest/src/main/java/com/sysonetest/service/AutomovilService.java
@@ -1,6 +1,7 @@
 package com.sysonetest.service;
 
 import com.sysonetest.entity.Automovil;
+import com.sysonetest.entity.Comodidad;
 import com.sysonetest.repository.AutomovilRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,44 +10,48 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service("automovilService")
 public class AutomovilService {
-    
+
     private final AutomovilRepository automovilRepository;
+
     @Autowired
     public AutomovilService(AutomovilRepository automovilRepository) {
         this.automovilRepository = automovilRepository;
     }
+
     public Automovil save(Automovil automovil) {
-		return this.automovilRepository.save(automovil);
-	}
-	
-	@Transactional(readOnly = true)
-	public List<Automovil> findAll() {
-		return this.automovilRepository.findAll();
-	}
-	
-	@Transactional(readOnly = true)
-	public Automovil findById(Long id) {
-		return this.automovilRepository.findById(id).orElse(null);
-	}
-	
-	@Transactional(readOnly = true)
-	public Automovil findAutomovilByNombre(String nombre) {
-		return this.automovilRepository.findAutomovilBynombre(nombre);
-	}
+        return this.automovilRepository.save(automovil);
+    }
 
-	public Automovil saveAutomovil(Automovil automovilRequest) {
+    @Transactional(readOnly = true)
+    public List<Automovil> findAll() {
+        return this.automovilRepository.findAll();
+    }
 
-		return this.automovilRepository.save(automovilRequest);
-	}
+    @Transactional(readOnly = true)
+    public Automovil findById(Long id) {
+        return this.automovilRepository.findById(id).orElse(null);
+    }
+
+    @Transactional(readOnly = true)
+    public Automovil findAutomovilByNombre(String nombre) {
+        return this.automovilRepository.findAutomovilBynombre(nombre);
+    }
+
+    public Automovil saveAutomovil(Automovil automovilRequest) {
+
+        return this.automovilRepository.save(automovilRequest);
+    }
+    public void deleteAutomovil(Automovil automovilRequest) {
+        List<Comodidad> lista = automovilRequest.getComodidades();
+        this.automovilRepository.delete(automovilRequest);
+    }
 
     public Automovil updateAutomovil(Automovil automovilActual, Automovil automovil) {
-		automovilActual.setNombre(automovil.getNombre());
-		automovilActual.setComodidades(automovil.getComodidades());
-		automovilActual.setPrecioBasico(automovil.getPrecioBasico());
-		automovilActual.setPrecioFinal(automovil.getPrecioFinal());
-		return automovilRepository.save(automovilActual);
+        automovilActual.setNombre(automovil.getNombre());
+        automovilActual.setComodidades(automovil.getComodidades());
+        automovilActual.setPrecioBasico(automovil.getPrecioBasico());
+        automovilActual.setPrecioFinal(automovil.getPrecioFinal());
+        return automovilRepository.save(automovilActual);
     }
-    
-    
-    
+
 }
