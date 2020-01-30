cd89e2c840f5fd1e4fc58253e2f1ca60ab767f6d
garofoloh <41156618+garofoloh@users.noreply.github.com>
2020-01-30 07:53:16 -0300

    comodidades

--- SysoneTest/src/main/java/com/sysonetest/entity/Comodidades.java
+++ SysoneTest/src/main/java/com/sysonetest/entity/Comodidades.java
@@ -1,68 +1,30 @@
 package com.sysonetest.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-
+@Entity
+@Table(name = "comodidades")
 public class Comodidades {
-    TechoCorredizo techo;
-    AireAcondicionado aire;
-    FrenosAbs frenos;
-    Airbag airbag;
-    LlantasAleacion llantas;
+    String nombre;
+    int precio;
+    
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "comodidadesID")
     private Long id;
 
     public Comodidades() {
     }
 
-    public TechoCorredizo getTecho() {
-        return techo;
-    }
-
-    public void setTecho(TechoCorredizo techo) {
-        this.techo = techo;
-    }
-
-    public AireAcondicionado getAire() {
-        return aire;
-    }
-
-    public void setAire(AireAcondicionado aire) {
-        this.aire = aire;
-    }
-
-    public FrenosAbs getFrenos() {
-        return frenos;
-    }
-
-    public void setFrenos(FrenosAbs frenos) {
-        this.frenos = frenos;
-    }
-
-    public Airbag getAirbag() {
-        return airbag;
-    }
-
-    public void setAirbag(Airbag airbag) {
-        this.airbag = airbag;
-    }
-
-    public LlantasAleacion getLlantas() {
-        return llantas;
-    }
-
-    public void setLlantas(LlantasAleacion llantas) {
-        this.llantas = llantas;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
+    public Comodidades(String nombre, int precio) {
+        this.nombre = nombre;
+        this.precio = precio;
     }
     
+
 }
